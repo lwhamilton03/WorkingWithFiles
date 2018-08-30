0a9ca6269b360b5b8519ec3a0adf415ca16e8efa
lwhamilton03 <lucy.hamilton@academytrainee.com>
2018-08-30 12:45:12 +0100

    updated to read and write

--- src/MainRunnerFiles.java
+++ src/MainRunnerFiles.java
@@ -4,14 +4,21 @@ public class MainRunnerFiles {
 	public static void main(String[] args) {
 		
 		WorkingFiles pepl = new WorkingFiles();
-		pepl.enterList("Henry", "Teacher", 23);
-		pepl.enterList("Frank", "Plumber", 32);
-		pepl.enterList("Janet", "Chef", 62);
-		pepl.enterList("Gemma", "Dog Trainer", 17);
-		pepl.enterList("Jerald", "Designer", 27);
+		//pepl.enterList("Henry", "Teacher", 23);
+		//pepl.enterList("Frank", "Plumber", 32);
+		//pepl.enterList("Janet", "Chef", 62);
+		//pepl.enterList("Gemma", "Dog Trainer", 17);
+		//pepl.enterList("Jerald", "Designer", 27);
 		
+		// pepl.printList();
+		
+		
+		// pepl.writeFile();
+		
+		// pepl.makeFile("AgeAbove18.txt");
+		
+		pepl.readFile();
 		pepl.printList();
-		 
 	}
 
 }
--- src/WorkingFiles.java
+++ src/WorkingFiles.java
@@ -1,3 +1,8 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.ArrayList;
 
 public class WorkingFiles {
@@ -25,4 +30,97 @@ public class WorkingFiles {
 		}
 	}
 	
+	public void writeFile()
+	{
+		File peopleFile = new File("listPeople.txt");
+		
+		try 
+		{
+			FileWriter fw = new FileWriter(peopleFile);
+			BufferedWriter bw = new BufferedWriter(fw);
+			
+			for(Person pep : peopleList)
+			{
+				bw.write(pep.toString());
+				bw.newLine();
+			}
+			bw.flush();
+			fw.close();
+			bw.close();
+		}
+		
+		catch(Exception except)
+		{
+			System.out.println(except);
+		}
+		
+	//	finally
+	//	{
+	//		
+	//	}
+	// in this case put the fw. close and the bw. close in here also
+		
+	}
+	
+	
+	public void makeFile(String nameOfFile)
+	{
+		File agePeople = new File(nameOfFile);
+		
+		FileWriter ff; 
+		BufferedWriter bb; 
+		
+		try
+		{
+			ff = new FileWriter(agePeople); 
+			bb = new BufferedWriter(ff);
+			
+			for(Person ppl : peopleList)
+			{
+				if (ppl.getAge() > 18)
+				{
+					bb.write(ppl.toString());
+					bb.newLine();
+				}
+		
+			}
+
+			bb.flush();
+			bb.close();
+			ff.close();
+		}
+		
+		catch(Exception excee)
+		{
+			System.out.println(excee);
+		}
+	
+	}
+	
+	public void readFile()
+	{
+		try
+		{
+			FileReader fr = new FileReader ("AgeAbove18.txt");
+			BufferedReader br = new BufferedReader(fr);
+			String list; 
+			
+			while((list = br.readLine()) != null)
+			{
+				list = list.replaceAll(",", "");
+				String [] listPeoplee = list.split(" ");
+				
+				//for(int i = 0; i <= listPeoplee.length - 1; i++)
+				//{
+				//	System.out.println(listPeoplee[i].toString());
+				//}
+			
+				peopleList.add(new Person(listPeoplee[1], listPeoplee[3], Integer.parseInt(listPeoplee[5])));
+			}
+		}
+		catch(Exception excepp)
+		{
+			System.out.println(excepp);
+		}
+	}
 }
